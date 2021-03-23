359660d4c601bdfb1891d3c8c7be47524f6433c1
Soham-1111 <73647733+Soham-1111@users.noreply.github.com>
2021-03-23 13:39:34 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -21,3 +21,26 @@ function addroomname(){
     });
     window.location= "kwitter_page.html";
 }
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+  Room_names = childKey;
+  //Start code
+ console.log(Room_names);
+ row= "<div class='room_name' id="+ Room_names +" onclick='redirect(this.id)'> #"+ Room_names + "</div><hr>";
+document.getElementById("output").innerHTML += row;
+
+ //End code
+ });});}
+getData();
+
+function redirect(name){
+console.log(name);
+localStorage.setItem("room_name", name);
+window.location="kwitter_page.html";
+}
+
+function logout(){
+localStorage.removeItem("user_name");
+localStorage.removeItem("room_name");
+window.location= "index.html";
+}
